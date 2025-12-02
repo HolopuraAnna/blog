250f9ee2660230d22398e4286aa4845883c311e7
Holopura Anna <holopurana@knu.ua>
2025-12-02 23:34:16 +0200

    add 3 latest posts

--- app/blog/views.py
+++ app/blog/views.py
@@ -5,11 +5,14 @@ def index(request):
     post_num = Post.objects.count()
     author_num = Author.objects.count()
     comment_num = Comment.objects.count()
+
+    latest_posts = Post.objects.order_by('-created_at')[:3]
     
     context = {
         'post_num': post_num,
         'author_num': author_num,
         'comment_num': comment_num,
+        'latest_posts': latest_posts,
     }
     
     return render(request, 'blog/index.html', context)
