2c17da7b7bc4e5c7013f30c75dab3f93c07a97d4
Holopura Anna <holopurana@knu.ua>
2025-12-02 22:59:56 +0200

    update index

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Author, Comment
 
 def index(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/index.html', {'posts': posts})
+    post_num = Post.objects.count()
+    author_num = Author.objects.count()
+    comment_num = Comment.objects.count()
+    
+    context = {
+        'post_num': post_num,
+        'author_num': author_num,
+        'comment_num': comment_num,
+    }
+    
+    return render(request, 'blog/index.html', context)
